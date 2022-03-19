4ce1293b49e61c6b2e8b437a292d05749ef3e277
Greg Buehler <gregory.r.buehler@gmail.com>
2022-03-19 09:15:06 -0700

    cleanup artifacts

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,13 +17,13 @@ mkdir -p $DIST_DIR
 dotnet clean
 
 # windows
-dotnet publish dpp.opentakrouter -c Release -r win10-x64 --self-contained=true 
+dotnet publish dpp.opentakrouter -c Release -r win10-x64 --self-contained=true -p:PublishSingleFile=true -p:PublishTrimmed=True -p:TrimMode=Link
 pushd ./dpp.opentakrouter/bin/Release/net5.0/win10-x64/publish/
 zip -r $DIST_DIR/opentakrouter-$VERSION-$HASH.zip .
 popd
 
 # linux
-dotnet publish dpp.opentakrouter -c Release -r linux-x64 --self-contained=true
+dotnet publish dpp.opentakrouter -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=true -p:PublishTrimmed=True -p:TrimMode=Link
 pushd ./dpp.opentakrouter/bin/Release/net5.0/linux-x64/publish/
 tar -czvf $DIST_DIR/opentakrouter-$VERSION-$HASH.tar.gz .
 popd
