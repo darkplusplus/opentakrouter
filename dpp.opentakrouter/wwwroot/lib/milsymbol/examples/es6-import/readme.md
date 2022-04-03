c41f1587b62adc7ae7a4b893d372dbe4bcb3e80d
Greg Buehler <gregory.r.buehler@gmail.com>
2022-04-02 18:05:26 -0700

    prune wwwroot lib

--- dpp.opentakrouter/wwwroot/lib/milsymbol/examples/es6-import/readme.md
+++
@@ -1,3 +0,0 @@
-## ES6 Imports
-
-This shows how milsymbol supports ES6 imports so that you can import only exactly what you need.
--- dpp.opentakrouter/wwwroot/lib/milsymbol/src/ms/colormode.js
+++
@@ -1,9 +0,0 @@
-export function Colormode(civilian, friend, hostile, neutral, unknown) {
-  var o = {};
-  o.Civilian = civilian;
-  o.Friend = friend;
-  o.Hostile = hostile;
-  o.Neutral = neutral;
-  o.Unknown = unknown;
-  return o;
-}
